40c2f4dee6bd8cba6a010f455927605ea16896de
MicaildeJunior <micaoliveira.tecnologo@gmail.com>
2023-08-11 07:07:02 -0300

    Terminado as Configurações na Program, foi criado métodos void de configuração

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -9,9 +9,9 @@ public static class Configuration
 
     public class SmtpConfiguration
     {
-        public string Host { get; set; }
+        public string? Host { get; set; }
         public int Port { get; set; } = 25;
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
     }
 }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -6,46 +6,15 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Obtem a chave de Bytes na Configuration
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-// Esquema de Autenticação padrão do AspNet, AddJwtBearer é a autorização pra usar o Token
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x => 
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true, // Validar chave de assinatura
-        IssuerSigningKey = new SymmetricSecurityKey(key), // Como valida? através de uma chave Simétrica
-        ValidateIssuer = false, // Esses dois é false, pq só está usando uma API nesse projeto
-        ValidateAudience = false
-    };
-});
-
-
-// Add services to the container.
-
-// Abaixo em .ConfigureApiBehaviorOptions, com isso não usa mais o !ModelState.IsValid na controller
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-// Adicionado para disponibilizar para todos os controllers
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenService>(); // Sempre cria um novo
-//builder.Services.AddScoped();    // Requisição, enquanto a request durar vai usar o token dela
-//builder.Services.AddSingleton(); // 1 por App, usa sempre a mesma instancia
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -53,19 +22,61 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
-
-var smtp = new Configuration.SmtConfiguration();
-app.Configuration.GetSection("Smtp").Bind(smtp);
-Configuration.Smtp = smtp;
+LoadConfiguration(app);
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    // Obtem a chave de Bytes na Configuration
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    // Esquema de Autenticação padrão do AspNet, AddJwtBearer é a autorização pra usar o Token
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true, // Validar chave de assinatura
+            IssuerSigningKey = new SymmetricSecurityKey(key), // Como valida? através de uma chave Simétrica
+            ValidateIssuer = false, // Esses dois é false, pq só está usando uma API nesse projeto
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    // Abaixo em .ConfigureApiBehaviorOptions, com isso não usa mais o !ModelState.IsValid na controller
+    builder.Services.AddControllers()
+        .ConfigureApiBehaviorOptions(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    // Adicionado para disponibilizar para todos os controllers
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>(); // Sempre cria um novo
+    //builder.Services.AddScoped();    // Requisição, enquanto a request durar vai usar o token dela
+    //builder.Services.AddSingleton(); // 1 por App, usa sempre a mesma instancia
+}
